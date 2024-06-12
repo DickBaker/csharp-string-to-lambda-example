81ddd5b4421d53b2ce6a5e6ec86f4975e147298b
DickBaker <dickbaker@totalise.co.uk>
2024-06-12 22:20:17 +0100

    refresh to C# 12
    see README.md for details

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,63 +1,59 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
-using Microsoft.CodeAnalysis.Scripting;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Net;
-using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
 
-namespace ConsoleApp6
+namespace ConsoleApp6;
+
+static class Program
 {
-    class Program
+    public static async Task Main()
     {
-        public static async Task Main(string[] args)
+        Console.WriteLine("Roslyn approach:");
+        const string discountFilter = "album => album.Quantity > 100";
+        ScriptOptions options = ScriptOptions.Default.AddReferences(typeof(Album).Assembly);
+        Func<Album, bool> discountFilterExpression = await CSharpScript.EvaluateAsync<Func<Album, bool>>(discountFilter, options);
+
+        var albums = new List<Album>
         {
-            Console.WriteLine("Roslyn approach:");
-            var discountFilter = "album => album.Quantity > 100";
-            var options = ScriptOptions.Default.AddReferences(typeof(Album).Assembly);
-            Func<Album, bool> discountFilterExpression = await CSharpScript.EvaluateAsync<Func<Album, bool>>(discountFilter, options);
+            new() { Quantity = 10, Artist = "Betontod", Title = "Revolution" },
+            new() { Quantity = 50, Artist = "The Dangerous Summer", Title = "The Dangerous Summer" },
+            new() { Quantity = 200, Artist = "Depeche Mode", Title = "Spirit" },
+        };
 
-            var albums = new List<Album>
-            {
-                new Album { Quantity = 10, Artist = "Betontod", Title = "Revolution" },
-                new Album { Quantity = 50, Artist = "The Dangerous Summer", Title = "The Dangerous Summer" },
-                new Album { Quantity = 200, Artist = "Depeche Mode", Title = "Spirit" },
-            };
-
-            var discountedAlbums = albums.Where(discountFilterExpression);
-
-            foreach(var album in discountedAlbums)
-            {
-                Console.WriteLine($"Discounted album: {album.Artist} - {album.Title}");
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("****");
-            Console.WriteLine();
-
-            // approach with manual expression building
-            Console.WriteLine("Manual expression building approach:");
-            var parameter = Expression.Parameter(typeof(Album), "album");
-            var comparison = Expression.GreaterThan(Expression.Property(parameter, Type.GetType("ConsoleApp6.Album").GetProperty("Quantity")), Expression.Constant(100));
-            var lambda = Expression.Lambda<Func<Album, bool>>(comparison, parameter).Compile();
-            var discountedAlbums2 = albums.Where(lambda);
-
-            foreach (var album in discountedAlbums2)
-            {
-                Console.WriteLine($"Discounted album: {album.Artist} - {album.Title}");
-            }
-
-            Console.ReadKey();
+        foreach (Album album in albums.Where(discountFilterExpression))
+        {
+            Console.WriteLine($"Discounted album: {album.Artist,-20} - {album.Title}");
         }
-    }
 
-    public class Album
-    {
-        public int Quantity { get; set; }
-        public string Title { get; set; }
-        public string Artist { get; set; }
+        Console.WriteLine("\n****\n");
+
+        // approach with manual expression building
+        Console.WriteLine("Manual expression building approach:");
+        ParameterExpression parameter = Expression.Parameter(
+            typeof(Album), "album");
+        BinaryExpression comparison = Expression.GreaterThan(Expression.Property(
+            parameter,
+            typeof(Album)
+                .GetProperty(nameof(Album.Quantity))!),
+            Expression.Constant(100));
+        Func<Album, bool> lambda = Expression.Lambda<Func<Album, bool>>(
+            comparison, parameter).Compile();
+        foreach (Album album in albums.Where(lambda))
+        {
+            Console.WriteLine($"Discounted album: {album.Artist,-20} - {album.Title}");
+        }
+
+        _ = Console.ReadKey();
     }
+}
+
+public class Album
+{
+    public required int Quantity { get; set; }
+    public required string Title { get; set; }
+    public required string Artist { get; set; }
 }
---
+++ README.md
@@ -0,0 +1,44 @@
+Filip Wojcieszyn (aka Filip W) Filip is
+- a cloud architect from Zürich
+- founder and maintainer of several popular open source projects
+- active contributor to the Q# community and a Microsoft Quantum MVP
+- specializes in programming language tooling, the C# compiler and quantum computing
+- based in Zürich, where he works in the medical industry, for Sonova AG, as an Architect in Research & Development.
+
+The "Easy way to create a C# lambda expression from a string (with Roslyn)" article on Filip's website
+https://www.strathweb.com/2018/01/easy-way-to-create-a-c-lambda-expression-from-a-string-with-roslyn/
+https://github.com/filipw/csharp-string-to-lambda-example
+
+is backed by the related [filipw/csharp-string-to-lambda-example](https://github.com/filipw/csharp-string-to-lambda-example) repository
+also originally released in 2018, with subsequent updates, although main Program.cs code has been unchanged.
+
+| date | TFM | Microsoft.CodeAnalysis.CSharp.Scripting |
+|-|-|-|
+| 2018-01-29 | netcoreapp2.0 | 2.6.1 |
+| 2018-07-18 | netcoreapp2.0 | 3.2.0-beta4-final |
+| 2021-03-24 | netcoreapp3.1 | 3.9.0 |
+| 2021-05-21 | netcoreapp3.1 | 3.10.0-2.final |
+| 2022-09-19 | net6.0 | 4.4.0-1.final |
+| 2023-03-07 | net6.0 | 4.6.0-1.final |
+
+This PR by Dick Baker (developer in UK) now contributes further updates per C# 12 and .NET 8
+
+| date | TFM | Microsoft.CodeAnalysis.CSharp.Scripting |
+|-|-|-|
+| 2023-03-07 | net8.0 | 4.10.0 |
+
+FYI there is an excellent "[Expression Trees in C#](https://app.pluralsight.com/library/courses/c-sharp-10-expression-trees/)" course on Pluralsight [original Nov 2022, updated Mar 2024] by [Chris B Behrens](https://app.pluralsight.com/profile/author/chris-behrens) who has authored 69 courses [including 11 now retired] at time of writing this (June 2024).
+Current PS subscribers can obtain the [Exercise files](https://app.pluralsight.com/library/courses/c-sharp-10-expression-trees/exercise-files) download that includes both code and .PDF files only
+- understanding-where-expressions-come-from-slides.pdf
+- understanding-expression-trees-in-c-slides.pdf
+- making-expression-trees-work-for-you-slides.pdf
+- Course Links.rtf
+
+where that .rtf file contains several short-links including [one](https://bit.ly/37STNmk) to Filip's article
+(no, I couldn't relate that M4 Slide 29 to any slide in the .PDF decks or transcript either!)
+
+Chris' public repository with code for this course is at https://github.com/FeynmanFan/csharp-expression-trees
+
+Exploring Filip's website I discovered he has also written a book, released 2022-05-07
+[Introduction to Quantum Computing with Q# and QDK](https://link.springer.com/book/10.1007/978-3-030-99379-5)
+so worthwhile to explore both website and book for more great insights !
